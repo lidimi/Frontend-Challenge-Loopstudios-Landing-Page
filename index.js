e910356fe6e910f4f7c34519220c43ec773a6743
lidimi <lidsheq97@gmail.com>
2022-12-07 18:24:01 +0100

    mobile menu improvment :iphone:

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const btnImg = document.querySelector('.btn-burger');
 
 button.addEventListener('click', () => {
     menu.classList.toggle('mobile-menu');
+    if (btnImg.getAttribute('src') === 'images/icon-hamburger.svg') {
+        btnImg.src = 'images/icon-close.svg';
+    } else {
+        btnImg.src = 'images/icon-hamburger.svg';
+    }
 });
-
